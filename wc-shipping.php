2281984b5fb3d9b6f34d7fc2b2ef455124a0f03f
Omonzejele Precious <crispusomonze@gmail.com>
2018-11-15 21:22:51 +0100

    added the new sets, unset lagos

--- wc-shipping.php
+++ wc-shipping.php
@@ -1,11 +1,14 @@
 add_filter('woocommerce_states', 'add_custom_states_to_country');
 add_filter('woocommerce_countries_allowed_country_states', 'add_custom_states_to_country');
 function add_custom_states_to_country( $states ) {
-    $states['NG'] = array(
+    $addition = array(
         'LC' => __('Lagos Central', 'woocommerce'),
         'LI' => __('Lagos Island', 'woocommerce'),
         'LE' => __('Lagos Island Edge', 'woocommerce'),
         'ME' => __('Mainland Edge', 'woocommerce')
         );
-    return $states;
+	//unset lagos
+	unset($states['NG']['LA']); 
+	$new_states = array_merge($states['NG'],$addition);
+    return $new_states;
 }
